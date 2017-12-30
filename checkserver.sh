078a45f5c27f0a3b1c9bbe0866260495f111bccb
Orestis Roditis <orestarod@gmail.com>
2017-12-30 19:55:09 +0200

    fixed identation on checkserver

--- checkserver.sh
+++ checkserver.sh
@@ -1,22 +1,22 @@
 ADDR="127.0.0.1"
-printf '>>Registering orestarod\n    '
+printf '>> Registering orestarod\n    '
 wget -qO- http://$ADDR:5000/register/123/orestarod
-printf '\n>>Registering airmper\n    '
+printf '\n>> Registering airmper\n    '
 wget -qO- http://$ADDR:5000/register/123/airmper
-printf '\n>>orestarod joining tanalies\n    '
+printf '\n>> orestarod joining tanalies\n    '
 wget -qO- http://$ADDR:5000/join_group/tanalies/orestarod
-printf '\n>>airmper joining tanalies\n    '
+printf '\n>> airmper joining tanalies\n    '
 wget -qO- http://$ADDR:5000/join_group/tanalies/airmper
-printf '\n>>listing tanalies members\n    '
+printf '\n>> listing tanalies members\n    '
 wget -qO- http://$ADDR:5000/list_members/tanalies
-printf '\n>>listing groups\n    '
+printf '\n>> listing groups\n    '
 wget -qO- http://$ADDR:5000/list_groups/
-printf '\n>>orestarod quitting\n    '
+printf '\n>> orestarod quitting\n    '
 wget -qO- http://$ADDR:5000/quit/0
-printf '\n>>listing tanalies members\n    '
+printf '\n>> listing tanalies members\n    '
 wget -qO- http://$ADDR:5000/list_members/tanalies
-printf '\n>>airmper quitting\n    '
+printf '\n>> airmper quitting\n    '
 wget -qO- http://$ADDR:5000/quit/1
-printf '\n>>listing all groups\n    '
+printf '\n>> listing all groups\n    '
 wget -qO- http://$ADDR:5000/list_groups/
 printf '\n'
