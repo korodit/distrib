ad8a2832616d71e4806ce809d24b289c3b02103a
Orestis Roditis <orestarod@gmail.com>
2018-03-08 20:29:05 +0200

    updated run client example comment

--- run_client.sh
+++ run_client.sh
@@ -4,4 +4,4 @@
 # python source/client/client.py $1 $2 $3 $4
 python source/client/client.py "$@"
 deactivate
-# bash run_client.sh udp=5006 register=orestarod room=loules mode=total/fifo msgfile=messages/messages1.txt start=1520531370.32 benchname=testbench
+# bash run_client.sh udp=5006 register=orestarod room=loules mode=total/fifo msgfile=messages/messages1.txt start=1520531370.32 benchname=testbench server=distrib-1
