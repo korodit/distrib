45b43a3d262593744bd2f7ab5867ddd1fadb6060
Orestis Roditis <orestarod@gmail.com>
2018-03-09 16:21:35 +0200

    copy to all bash script

---
+++ copy_to_all.sh
@@ -0,0 +1,4 @@
+scp -r /home/distrib04/distrib/ distrib-2:/home/distrib04/distrib/
+scp -r /home/distrib04/distrib/ distrib-3:/home/distrib04/distrib/
+scp -r /home/distrib04/distrib/ distrib-4:/home/distrib04/distrib/
+scp -r /home/distrib04/distrib/ distrib-5:/home/distrib04/distrib/
